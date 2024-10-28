037d3eaf0c229f0358d28f93d015baac34616656
PratimaR6 <pratimavrcs2019@gmail.com>
2024-10-28 21:55:22 +0530

    Delete routes directory

--- routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/weatherRoutes.js
-const express = require('express');
-const router = express.Router();
-const weatherController = require('../controllers/weatherController');
-
-router.post('/update', weatherController.updateWeatherData);
-
-module.exports = router;
